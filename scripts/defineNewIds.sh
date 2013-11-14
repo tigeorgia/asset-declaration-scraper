2311eaa8a817f141657dec2f5a48e5cdbc9c95c8
Etienne Baque <etiennebaque@gmail.com>
2013-11-14 15:26:00 +0400

    Fixed generation of id list

--- scripts/defineNewIds.sh
+++ scripts/defineNewIds.sh
@@ -20,6 +20,10 @@ cat $1 | sort -g | sed '/^$/d' > sortedidlist
 
 # Read the old list of id, make a diff with the new list, and see what ids are really new.
 echo "Crawling new list of id from declaration.gov.ge now..."
+if [ -f "$OUTPUT/idlist" ]; then
+    rm $OUTPUT/idlist
+fi
+
 ./scripts/defineDeclarationIds.sh
 
 cat $OUTPUT/idlist | sort -g | sed '/^$/d' > sortednewidlist
@@ -33,3 +37,5 @@ cp ./newids "$PATH_TO_SCRAPPER"/idlist
 # Timestamp is added to the name of new list of ids file.
 now=$(date +'%Y-%m-%d')
 cp $BASEDIR/sortednewidlist $BASEDIR/"declarationids-"$now
+
+rm sortednewidlist
